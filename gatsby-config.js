0d1fe5f5670609dd82e15ba10e7e098d3a5c8af5
Thilo Maier <git@maier.asia>
2019-05-29 14:12:09 -0400

    Update project configuration (closes #4)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "United Nations World Data Forum",
+    title: 'United Nations World Data Forum',
     description:
-      "The 3rd UN World Data Forum will take place from 18-21 October 2020 in Bern, Switzerland.",
-    url: "https://next.undataforum.org"
+      'The 3rd UN World Data Forum will take place from 18-21 October 2020 in Bern, Switzerland.',
+    url: 'https://next.undataforum.org',
   },
   plugins: [
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "content",
-        path: `${__dirname}/content/`
-      }
+        name: 'content',
+        path: `${__dirname}/content/`,
+      },
     },
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        excerpt_separator: "<!-- end -->",
-        plugins: ["gatsby-remark-autolink-headers", "gatsby-remark-smartypants"]
-      }
-    },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-blog",
-    "gatsby-plugin-profiles",
-    "gatsby-plugin-react-helmet"
-  ]
+  ],
 };
