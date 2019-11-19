e922b2f5c6a3eacfde100651126508803cfd4c5e
Thilo Maier <git@maier.asia>
2019-11-19 17:59:14 -0500

    Fix description meta tags GH-120

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -3,16 +3,24 @@ module.exports = {
     title: 'United Nations World Data Forum',
     siteUrl: 'https://undataforum.org',
     description:
-      'The 3rd United Nations World Data Forum will take place from 18-21 October 2020 in Bern, Switzerland.',
+      'The 3rd United Nations World Data Forum will take place 18-21 October 2020 in Bern, Switzerland.',
   },
   plugins: [
     {
       resolve: '@undataforum/gatsby-theme-blog',
-      options: { basePath: '/blog' },
+      options: {
+        basePath: '/blog',
+        description:
+          'Blog authored by the United Nations World Data Forum community.',
+      },
     },
     {
       resolve: '@undataforum/gatsby-theme-events',
-      options: { basePath: '/events' },
+      options: {
+        basePath: '/events',
+        description:
+          'Events in preparation of the 2020 United Nations World Data Forum.',
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
