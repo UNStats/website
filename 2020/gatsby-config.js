ae0c3aa73d5ef14443e2f3a6a6c8cbdfaa5de75f
Thilo Maier <git@maier.asia>
2019-11-19 12:18:07 -0500

    Add 2020 website GH-124

---
+++ 2020/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: '2020 United Nations World Data Forum',
+    siteUrl: 'https://undataforum.org/2020',
+    description:
+      'The 3rd United Nations World Data Forum took place 18-21 October 2020 in Bern, Switzerland',
+  },
+  plugins: [
+    {
+      resolve: '@undataforum/gatsby-theme-profiles',
+      options: {},
+    },
+  ],
+};
